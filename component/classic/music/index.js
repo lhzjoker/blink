b7fd6ddbe4dd97d253e3cb01a84722843e9e797e
lhzjoker <2313169248@qq.com>
2020-03-26 17:06:10 +0800

    设置音乐组件的播放暂停等功能

--- component/classic/music/index.js
+++ component/classic/music/index.js
@@ -1,27 +1,98 @@
 // component/classic/music/index.js
 import {classicBeh} from "../classic-Beh";
 
+/*获取背景音频管理器*/
+const mMgr = wx.getBackgroundAudioManager()
+
 Component({
   /**
    * 组件的属性列表
    */
   behaviors:[classicBeh],   /*里面可以有多个behavior，多个behavior中如果有相同的属性，去最后一个behavior的值*/
-  properties: {             /*properties可以覆盖behavior中的相同属性*/
+  properties: {
+     src: String,
+     title: String
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-    off: 'images/player@pause.png',
-    on: 'images/player@play.png',
+    playing: false,
+    pauseSrc: 'images/player@pause.png',
+    playSrc: 'images/player@play.png',
     tag: 'images/music@tag.png'
+  },
+
+  /*注意：hidden不执行任何生命周期函数，只有wx:if执行*/
+  /*页面生成时执行*/
+  attached: function(event){
+    this._recoverStatus()
+    this._monitorSwitch()
+  },
+
+  //在组件实例被从页面节点树移除时执行
+  detached: function(event){
+    //wx:if 和hidden的区别要搞清
+    //这里使用wx:if将重新渲染，切换时变成默认状态，并停止音乐，用hidden将不会，hidden只是简单的隐藏和显示
+    //mMgr.stop()因为我们播放音乐的时候需要设置后台播放，不能设置成切换页面就停止音乐
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+    onPlay: function (event) {
+      if(!this.data.playing){
+        this.setData({
+          playing: true
+        });
+        mMgr.src = this.properties.src  /*获取src链接将自动播放*/
+        mMgr.title=this.properties.title  /*音频标题，用于原生音频播放器音频标题（必填）*/
+      }
+      else{
+        this.setData({
+          playing: false
+        });
+        mMgr.pause()
+      }
+    },
 
+    /*判断播放状态，并显示对应图标*/
+    _recoverStatus: function(){
+      /*判断音乐是否暂停，如果暂停，切换页面时显示停止状态*/
+      if(mMgr.paused){
+        this.setData({
+          playing: false
+        })
+        return
+      }
+      /*判断音乐是否播放，如果播放，切换页面时显示播放状态*/
+      if(mMgr.src==this.properties.src){
+        this.setData({
+          playing: true
+        })
+      }
+    },
+
+    /*播放管理器按钮与小程序按钮状态同步*/
+    _monitorSwitch: function(){
+      //播放
+      mMgr.onPlay(()=>{
+        this._recoverStatus()
+      })
+      //暂停
+      mMgr.onPause(()=>{
+        this._recoverStatus()
+      })
+      //关闭播放器
+      mMgr.onStop(()=>{
+        this._recoverStatus()
+      })
+      //当前歌曲播放完毕
+      mMgr.onEnded(()=>{
+        this._recoverStatus()
+      })
+    }
   }
 })
--- pages/classic/classic.js
+++ pages/classic/classic.js
@@ -5,112 +5,112 @@ import {LikeModel} from "../../models/like";
 
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    classic: null,
-    latest: true,
-    first: false,
-    likeCount: 0,
-    likeStatus: false
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        classic: null,
+        latest: true,
+        first: false,
+        likeCount: 0,
+        likeStatus: false
+    },
 
-  /*监听点赞事件返回服务端*/
-  onLike: function(event){
-    console.log(event);
-    LikeModel.getLike(event.detail.status, this.data.classic.id, this.data.classic.type)
-  },
-  /*监听切换页面事件*/
-  onNext: function(){
-    this._updateClassic('next')
-  },
-  onPrevious: function(){
-      this._updateClassic('previous')
-  },
-  /*更新页面函数*/
-  _updateClassic: function(nextOrprevious){
-    ClassicModel.getCLassic(this.data.classic.index,nextOrprevious,(res)=>{
-      this._getLikeStatus(res.id,res.type)
-      this.setData({
-        classic: res,
-        latest: ClassicModel.islatest(res.index),
-        first: ClassicModel.isFirst(res.index)
-      })
-    })
-  },
-  /*获取页面的点赞状态信息*/
-  _getLikeStatus(artID,category){
-    LikeModel.getClassicLikeStatus(artID,category,(res)=> {
-      /*更新点赞数和点赞状态，因为这个是实时更新的，不能写入缓存当中，需要额外更新*/
-      this.setData({
-        likeCount: res.fav_nums,
-        likeStatus: res.like_status
-      })
-    })
-  }
-  ,
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-      ClassicModel.getLatest((res)=>{
-        // this._getLikeStatus(res.id,res.type)
-        /*数据更新*/
-        this.setData({
-          classic: res,
-          likeCount: res.fav_nums,
-          likeStatus: res.like_status
+    /*监听点赞事件返回服务端*/
+    onLike: function (event) {
+        console.log(event);
+        LikeModel.getLike(event.detail.status, this.data.classic.id, this.data.classic.type)
+    },
+    /*监听切换页面事件*/
+    onNext: function () {
+        this._updateClassic('next')
+    },
+    onPrevious: function () {
+        this._updateClassic('previous')
+    },
+    /*更新页面函数*/
+    _updateClassic: function (nextOrprevious) {
+        ClassicModel.getCLassic(this.data.classic.index, nextOrprevious, (res) => {
+            this._getLikeStatus(res.id, res.type)
+            this.setData({
+                classic: res,
+                latest: ClassicModel.islatest(res.index),
+                first: ClassicModel.isFirst(res.index)
+            })
         })
-      })
-  },
+    },
+    /*获取页面的点赞状态信息*/
+    _getLikeStatus(artID, category) {
+        LikeModel.getClassicLikeStatus(artID, category, (res) => {
+            /*更新点赞数和点赞状态，因为这个是实时更新的，不能写入缓存当中，需要额外更新*/
+            this.setData({
+                likeCount: res.fav_nums,
+                likeStatus: res.like_status
+            })
+        })
+    }
+    ,
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        ClassicModel.getLatest((res) => {
+            // this._getLikeStatus(res.id,res.type)
+            /*数据更新*/
+            this.setData({
+                classic: res,
+                likeCount: res.fav_nums,
+                likeStatus: res.like_status
+            })
+        })
+    },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
 
-  },
+    },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
 
-  },
+    },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
 
-  },
+    },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
 
-  }
+    }
 })
